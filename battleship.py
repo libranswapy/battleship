261e5806ba2e7e4faaca7e2815e23d202dd4d819
Swapnil Dukhande <libranswapy@gmail.com>
2014-07-22 23:44:37 -0700

    battleship.py
    Battleship game written in Python

---
+++ battleship.py
@@ -0,0 +1,51 @@
+from random import randint
+
+# Initializes empty board
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+# Flattens the board
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Let's play Battleship!"
+print_board(board)
+
+# Generate random numbers for row and column
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+
+# Every player has 4 turns and prints the turns after taking user input
+for turn in range(4):
+    print "Turn: " + str(turn+1)
+    guess_row = int(raw_input("Guess Row:"))  
+    guess_col = int(raw_input("Guess Col:"))
+
+# After correct guess the games gets over 
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        break
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."           
+        else:
+            print "You missed the battleship!"
+            print_board(board)
+    if turn == 3:
+        print "Game Over! Thanks for playing"
+        print (turn + 1)
+       
+    
